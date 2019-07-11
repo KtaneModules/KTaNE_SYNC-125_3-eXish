c1fc847e34e2f94762e9f185ae92df5980b58246
Brendan King <brendan@brendanking.com>
2019-07-10 22:41:17 -0400

    Added TP support

--- Assets/sync125_3.cs
+++ Assets/sync125_3.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using KMHelper;
+using System.Text.RegularExpressions;
 
 public class sync125_3 : MonoBehaviour
 {
@@ -12,14 +14,14 @@ public class sync125_3 : MonoBehaviour
     public TextMesh digitText;
     public MeshRenderer[] stageLights;
     public KMBombInfo info;
-    
+
     public Material off;
     public Material on;
-    
+
     string screenLabel;
-    
+
     int digit = 0;
-    
+
     string[] words = new[] {
         "İ'ms'", "ăĠ'n'", "kğ'i", "kĞ'p'", "ăut'",
         "ăġ'r", "ăġ'm", "ărs", "kğp'", "kğk",
@@ -30,7 +32,7 @@ public class sync125_3 : MonoBehaviour
         "Ğim", "ĆĠ'n'", "ĆČn'", "ĳim", "ĳip",
         "sğ'i", "ģif", "ģin", "ģir", "ğĂn'"
     };
-    
+
     string[] trans = new[] {
         "bombs", "calling", "clay", "club", "code",
         "color", "column", "course", "crab", "crack",
@@ -41,7 +43,7 @@ public class sync125_3 : MonoBehaviour
         "rhyme", "selling", "setting", "shame", "shape",
         "slay", "wife", "wine", "wire", "wrecking"
     };
-    
+
     int[] values = new[] {
         0, 8, 15, 7, -1,
         1, 9, 14, 6, -4,
@@ -52,11 +54,11 @@ public class sync125_3 : MonoBehaviour
         6, 14, 9, 1, -3,
         7, 15, 8, 0, -2
     };
-    
+
     int stage = 0;
     int textId = 0;
     bool isActive = false;
-    
+
     static int _moduleIdCounter = 1;
     int _moduleId = 0;
 
@@ -70,14 +72,14 @@ public class sync125_3 : MonoBehaviour
 
     void Init()
     {
-        for(int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < buttons.Length; i++)
         {
             int j = i;
             buttons[i].OnInteract += delegate () { OnPress(j); return false; };
         }
         submitButton.OnInteract += delegate () { OnSubmit(); return false; };
     }
-    
+
     IEnumerator Randomize(bool wait = false)
     {
         if (wait)
@@ -89,7 +91,7 @@ public class sync125_3 : MonoBehaviour
         int newTextId = UnityEngine.Random.Range(0, words.Length - 1);
         textId = newTextId + ((newTextId < textId) ? 0 : 1);
         screenText.text = words[textId];
-        
+
         Debug.LogFormat("[SYNC-125 [3] #{0}] Stage {1}. The word is \"{2}\".", _moduleId, stage + 1, trans[textId]);
         isActive = true;
     }
@@ -101,13 +103,13 @@ public class sync125_3 : MonoBehaviour
         StartCoroutine(Randomize());
         Init();
     }
-    
+
     IEnumerator StageLight(int st)
     {
         yield return new WaitForSeconds(0.15f);
         stageLights[stage++].material = on;
     }
-    
+
     void OnPress(int pressedButton)
     {
         GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
@@ -119,7 +121,7 @@ public class sync125_3 : MonoBehaviour
         digit = digit % 4 * 4 + pressedButton;
         digitText.text = Convert.ToString(digit, 16).ToUpper();
     }
-    
+
     void OnSubmit()
     {
         GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
@@ -190,6 +192,69 @@ public class sync125_3 : MonoBehaviour
             GetComponent<KMBombModule>().HandleStrike();
             isActive = false;
             StartCoroutine(Randomize(true));
+        }
+    }
+
+    //twitch plays
+    private bool cmdIsValid(string s)
+    {
+        char[] valids = { '0', '1', '2', '3' };
+        if(s.Length > 2)
+        {
+            return false;
+        }
+        foreach (char c in s)
+        {
+            if (!valids.Contains(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} submit <num> [Submits the specified number ONLY IF it is in base 4. The highest an answer can be is 2 digits]";
+    #pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        string[] parameters = command.Split(' ');
+        if (Regex.IsMatch(parameters[0], @"^\s*submit\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            if(parameters.Length == 2)
+            {
+                if (cmdIsValid(parameters[1]))
+                {
+                    yield return null;
+                    //for clearing
+                    buttons[0].OnInteract();
+                    buttons[0].OnInteract();
+                    //end clearing
+                    yield return new WaitForSeconds(0.1f);
+                    for (int i = 0; i < parameters[1].Length; i++)
+                    {
+                        if (parameters[1].ElementAt(i).Equals('0'))
+                        {
+                            buttons[0].OnInteract();
+                        }
+                        else if (parameters[1].ElementAt(i).Equals('1'))
+                        {
+                            buttons[1].OnInteract();
+                        }
+                        else if (parameters[1].ElementAt(i).Equals('2'))
+                        {
+                            buttons[2].OnInteract();
+                        }
+                        else if (parameters[1].ElementAt(i).Equals('3'))
+                        {
+                            buttons[3].OnInteract();
+                        }
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                    submitButton.OnInteract();
+                }
+            }
+            yield break;
         }
     }
 }
